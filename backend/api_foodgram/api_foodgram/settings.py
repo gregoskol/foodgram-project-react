c9a107996477faca273490e70decb090c39d7586
Антон Кондрашов <greg_oskol@inbox.ru>
2023-02-21 21:45:44 +0300

    rev1_02

--- backend/api_foodgram/api_foodgram/settings.py
+++ backend/api_foodgram/api_foodgram/settings.py
@@ -85,23 +85,23 @@ WSGI_APPLICATION = "api_foodgram.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
 # DATABASES = {
 #    "default": {
-#        "ENGINE": os.getenv("DB_ENGINE"),
-#        "NAME": os.getenv("DB_NAME"),
-#        "USER": os.getenv("POSTGRES_USER"),
-#        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-#        "HOST": os.getenv("DB_HOST"),
-#        "PORT": os.getenv("DB_PORT"),
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
 #    }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
 
 
 # Password validation
