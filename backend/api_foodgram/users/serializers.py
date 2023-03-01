8a2c7b9d6addb2fa5573cb8fdcebae2de126a5b9
Антон Кондрашов <greg_oskol@inbox.ru>
2023-03-01 17:49:06 +0300

    front

--- backend/api_foodgram/users/serializers.py
+++ backend/api_foodgram/users/serializers.py
@@ -83,8 +83,8 @@ class ShowSmallRecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ("id", "name", "image", "cooking_time")
-        read_only_fields = ("id", "name", "image", "cooking_time")
+        fields = ("id", "name", "image", "cooking_time", "author")
+        read_only_fields = ("id", "name", "image", "cooking_time", "author")
 
 
 class FollowSerializer(serializers.ModelSerializer):
