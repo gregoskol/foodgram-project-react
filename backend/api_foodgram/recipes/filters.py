ad36dd14145c02328af4844e05bb5caab458eac8
Антон Кондрашов <greg_oskol@inbox.ru>
2023-03-02 21:55:17 +0300

    front

--- backend/api_foodgram/recipes/filters.py
+++ backend/api_foodgram/recipes/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet, filters
 
-from recipes.models import Ingredient, Recipe
+from recipes.models import Ingredient, Recipe  # isort: skip
 
 
 class IngredientsFilter(FilterSet):
